858f0ff76f5009f04d084316e9d9f042f0c1737a
Diego Alonso Molina <diegoalonso@tuniversformacion.es>
2024-01-15 09:10:15 +0100

    Tenemos en el encabezado del js

--- htdocs/sala_chat/js/client.js
+++ htdocs/sala_chat/js/client.js
@@ -2,3 +2,16 @@
  * @author Diego Alonso Molina (Full Stack Developer)
  * @github https://github.com/DiegusNueva/conexion-js-websocket-php
  */
+
+// Funciones de utilidad
+const $ = id => { return document.getElementById(id) }
+const log = msg =>{
+    $("log").innerHTML += "<br>" + msg
+    // Cada vez que escribimos un mensaje, hacemos scroll vertical
+    $("log").scrollTop = $("log").scrollHeight
+}
+const onkey = event => { if (event.keyCode == 13) { send() } }
+
+// Variable global para instanciar el WebSocket
+
+let socket
